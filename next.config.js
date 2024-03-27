436e4186cc7cd1b6ac8e6ffe0d20b854305b62fc
Younes khadraoui <younes.khadraoui.pro@gmail.com>
2024-03-27 17:30:26 +0100

    [fix] deleted unused package

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
