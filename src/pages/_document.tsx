77e0ef1a4dcaaeec532ccf5e07f4656267e670ee
Younes-khadraoui <younes.khadraoui.pro@gmail.com>
2024-10-14 22:21:01 +0100

    added a meta tag for phones

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,7 @@ export default function Document() {
           content="Younes Khadraoui, web developer, portfolio"
         />
         <meta name="author" content="Younes Khadraoui" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
